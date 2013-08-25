c751429ce6954feb9e3a3bc992f8beade52e6960
Matt Stanford <mds6058@gmail.com>
2013-08-24 21:45:04 -0400

    Added Podfile, included AFNetworking

---
+++ HackerNews/ViewControllers/HNMenuVC.h
@@ -0,0 +1,13 @@
+//
+//  HNMenuVC.h
+//  HackerNews
+//
+//  Created by Matthew Stanford on 8/24/13.
+//  Copyright (c) 2013 Matthew Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNMenuVC : UIViewController
+
+@end
