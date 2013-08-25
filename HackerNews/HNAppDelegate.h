1f4d007fc92876c568252721d6e37f35b951ced7
Matt Stanford <mds6058@gmail.com>
2013-08-24 20:03:17 -0400

    Initial Commit

---
+++ HackerNews/HNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HNAppDelegate.h
+//  HackerNews
+//
+//  Created by Matthew Stanford on 8/24/13.
+//  Copyright (c) 2013 Matthew Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HackerNews/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HackerNews
+//
+//  Created by Matthew Stanford on 8/24/13.
+//  Copyright (c) 2013 Matthew Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HNAppDelegate class]));
+    }
+}
