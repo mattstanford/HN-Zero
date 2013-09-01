20c4d0de08cf6fbf300e03474b04c684c57c3bbb
Matt Stanford <mds6058@gmail.com>
2013-08-31 22:53:37 -0400

    Added new custom table cell class for article view

---
+++ HackerNews/Views/HNArticleCell.h
@@ -0,0 +1,13 @@
+//
+//  HNArticleCell.h
+//  HackerNews
+//
+//  Created by Matthew Stanford on 8/31/13.
+//  Copyright (c) 2013 Matthew Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNArticleCell : UITableViewCell
+
+@end
---
+++ HackerNews/Views/HNArticleCell.m
@@ -0,0 +1,29 @@
+//
+//  HNArticleCell.m
+//  HackerNews
+//
+//  Created by Matthew Stanford on 8/31/13.
+//  Copyright (c) 2013 Matthew Stanford. All rights reserved.
+//
+
+#import "HNArticleCell.h"
+
+@implementation HNArticleCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
