add1d82e11f3cb705ff22cd57366f55e2eaac257
Matt Stanford <mds6058@gmail.com>
2013-09-28 23:11:14 -0400

    Fixed bug where app would crash loading comment VC for article with 0 comments

--- HackerNews/Parser/HNCommentParser.m
+++ HackerNews/Parser/HNCommentParser.m
@@ -31,40 +31,49 @@
 
 + (NSArray *)getCommentRows:(NSData *)htmlData
 {
+    NSArray *commentRows = nil;
     TFHpple *parser = [TFHpple hppleWithHTMLData:htmlData];
     
     NSString *xpathQueryString = @"//table";
     NSArray *mainCommentPageElements = [parser searchWithXPathQuery:xpathQueryString];
     
-    TFHppleElement *mainCommentPageTable = [mainCommentPageElements objectAtIndex:0];
-    NSArray *mainCommentPageTableRows = [mainCommentPageTable children];
+    if (mainCommentPageElements) {
+        
+        TFHppleElement *mainCommentPageTable = [mainCommentPageElements objectAtIndex:0];
+        NSArray *mainCommentPageTableRows = [mainCommentPageTable children];
     
-    /*
-     The main comment section with the 'submit box' and the comments are index 2 in the table.  
-     Index 0 is the header, index 1 is the space between the header and the comments.
-     
-     This element has a single 'td' element in it that contains two more tables
-     */
-    TFHppleElement *tdWithCommentsAndSubmitBox = [[mainCommentPageTableRows objectAtIndex:2] firstChildWithTagName:@"td"];
+        
+        if (mainCommentPageTableRows && mainCommentPageTableRows.count >= 3) {
+            
+            /*
+             The main comment section with the 'submit box' and the comments are index 2 in the table.
+             Index 0 is the header, index 1 is the space between the header and the comments.
+             
+             This element has a single 'td' element in it that contains two more tables
+             */
+            TFHppleElement *tdWithCommentsAndSubmitBox = [[mainCommentPageTableRows objectAtIndex:2] firstChildWithTagName:@"td"];
+            
+            if (tdWithCommentsAndSubmitBox && tdWithCommentsAndSubmitBox.children.count > 4)
+            {
+                /*
+                 The TD with the comments and submit box has these elements as children:
+                 
+                  - Table (The submit box)
+                  - br
+                  - br
+                  - Table (The comments)
+                 
+                 Using this layout, we will get index 3 of the children of this element
+                 */
+                
+                TFHppleElement *commentsTable = [[tdWithCommentsAndSubmitBox children] objectAtIndex:3];
+                
+                commentRows = [commentsTable children];
+            }
+        }
+    }
     
-    /*
-     The TD with the comments and submit box has these elements as children:
-     
-      - Table (The submit box)
-      - br
-      - br
-      - Table (The comments)
-     
-     Using this layout, we will get index 3 of the children of this element
-     */
-    
-    TFHppleElement *commentsTable = [[tdWithCommentsAndSubmitBox children] objectAtIndex:3];
-    
-    /*
-     Now return the comment rows as an array
-     */
-    
-    return [commentsTable children];
+    return commentRows;
     
 }
 
