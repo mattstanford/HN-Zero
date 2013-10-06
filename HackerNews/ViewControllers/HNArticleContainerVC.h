b4b3d43852d5b888b3621d4af4f47847853859a3
Matt Stanford <mds6058@gmail.com>
2013-10-06 07:54:10 -0400

    Added container view for articles/comments

---
+++ HackerNews/ViewControllers/HNArticleContainerVC.h
@@ -0,0 +1,13 @@
+//
+//  HNArticleContainerVC.h
+//  HackerNews
+//
+//  Created by Matthew Stanford on 10/5/13.
+//  Copyright (c) 2013 Matthew Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNArticleContainerVC : UIViewController
+
+@end
