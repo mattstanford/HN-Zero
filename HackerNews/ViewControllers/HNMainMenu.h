8216e31306ee7f6dad7202b5f6a225ac0cb1e870
Matt Stanford <mds6058@gmail.com>
2013-10-15 23:50:20 -0400

    Added initial main menu VC

---
+++ HackerNews/ViewControllers/HNMainMenu.h
@@ -0,0 +1,13 @@
+//
+//  HNMainMenu.h
+//  HackerNews
+//
+//  Created by Matthew Stanford on 10/15/13.
+//  Copyright (c) 2013 Matthew Stanford. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HNMainMenu : UIViewController
+
+@end
---
+++ HackerNews/ViewControllers/HNMainMenu.m
@@ -0,0 +1,38 @@
+//
+//  HNMainMenu.m
+//  HackerNews
+//
+//  Created by Matthew Stanford on 10/15/13.
+//  Copyright (c) 2013 Matthew Stanford. All rights reserved.
+//
+
+#import "HNMainMenu.h"
+
+@interface HNMainMenu ()
+
+@end
+
+@implementation HNMainMenu
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
