9e1f015cd6b5cab4f6b2fb1994f8a3676b0afbc2
Matt Stanford <mds6058@yahoo.com>
2015-03-14 13:54:03 -0400

    Added back "Ask HN", "Show HN", "Jobs", and "New" sections to reflect the update to the HN API

--- HackerNews/HNAppDelegate.m
+++ HackerNews/HNAppDelegate.m
@@ -232,24 +232,28 @@
     NSString *frontPageUrlString = [NSString stringWithFormat:@"%@/topstories", urlBase];
     frontPage.url = [NSURL URLWithString:frontPageUrlString];
     
-  /*  HNMenuLink *askHN = [[HNMenuLink alloc] init];
+    HNMenuLink *askHN = [[HNMenuLink alloc] init];
     askHN.title = @"Ask HN";
-    askHN.url = [NSURL URLWithString:@"https://news.ycombinator.com/ask"];
+    NSString *askUrlString = [NSString stringWithFormat:@"%@/askstories", urlBase];
+    askHN.url = [NSURL URLWithString:askUrlString];
     
     HNMenuLink *showHN = [[HNMenuLink alloc] init];
     showHN.title = @"Show HN";
-    showHN.url = [NSURL URLWithString:@"https://news.ycombinator.com/show"];
+    NSString *showUrlString = [NSString stringWithFormat:@"%@/showstories", urlBase];
+    showHN.url = [NSURL URLWithString:showUrlString];
     
     HNMenuLink *jobsHN = [[HNMenuLink alloc] init];
     jobsHN.title = @"Jobs";
-    jobsHN.url = [NSURL URLWithString:@"https://news.ycombinator.com/jobs"];
+    NSString *jobsUrlString = [NSString stringWithFormat:@"%@/jobstories", urlBase];
+    jobsHN.url = [NSURL URLWithString:jobsUrlString];
     
     HNMenuLink *newHN = [[HNMenuLink alloc] init];
     newHN.title = @"New";
-    newHN.url = [NSURL URLWithString:@"https://news.ycombinator.com/newest"];*/
+    NSString *newUrlString = [NSString stringWithFormat:@"%@/newstories", urlBase];
+    newHN.url = [NSURL URLWithString:newUrlString];
     
-    //menuLinks = [[NSArray alloc] initWithObjects:frontPage, askHN, showHN, jobsHN, newHN, nil];
-    menuLinks = [[NSArray alloc] initWithObjects:frontPage, nil];
+    menuLinks = [[NSArray alloc] initWithObjects:frontPage, askHN, showHN, jobsHN, newHN, nil];
+    //menuLinks = [[NSArray alloc] initWithObjects:frontPage, nil];
 
     
     return menuLinks;
@@ -280,33 +284,6 @@
     appTheme.commentPostFont = [UIFont fontWithName:@"Helvetica" size:defaultFontSize];
     
     return appTheme;
-}
-
-- (void)applicationWillResignActive:(UIApplication *)application
-{
-    // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-    // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
-}
-
-- (void)applicationDidEnterBackground:(UIApplication *)application
-{
-    // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
-    // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-}
-
-- (void)applicationWillEnterForeground:(UIApplication *)application
-{
-    // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-}
-
-- (void)applicationDidBecomeActive:(UIApplication *)application
-{
-    // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-}
-
-- (void)applicationWillTerminate:(UIApplication *)application
-{
-    // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
 @end
